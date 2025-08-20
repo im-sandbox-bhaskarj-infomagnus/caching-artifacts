0682c6b6533d03aabd0b2c5a593afc2f18ef6e3a
bhaskarj-infomagnus <bhaskar.jalagam@infomagnus.com>
2025-08-20 12:44:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 def main():
     print("Hello from GitHub Actions caching & artifacts demo!")
     with open("output.txt", "w") as f:
-        f.write("This is a build artifact.\n")
+        f.write("This is a build artifact. New Chnage!!\n")
 
 if __name__ == "__main__":
     main()
